a622eec65af2c5c8dde50b1dc023d15dac999fb8
JAndrean29 <joshua.andrean29@gmail.com>
2025-02-04 13:17:20 +0700

    add Update User Handler & Usecase

--- handler/handler.go
+++ handler/handler.go
@@ -8,8 +8,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var req model.User
+
 func CreateUserHandler(c *gin.Context) {
-	var req model.User
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -32,4 +33,18 @@ func GetUserHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, users)
+}
+
+func UpdateUserHandler(c *gin.Context) {
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	affectedUser, err := usecase.UpdateUserUsecase(&req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": affectedUser})
 }
